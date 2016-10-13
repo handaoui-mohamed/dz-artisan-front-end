f65861961797988f51947279dcf8c59314177b22
Handaoui Mohamed <handaoui.mohamed@gmail.com>
2016-10-13 08:22:24 +0100

    set up the services page

--- src/app/search/search.controller.js
+++ src/app/search/search.controller.js
@@ -5,7 +5,15 @@
         .module('app.search')
         .controller('SearchController', SearchController);
 
-    function SearchController($log, SearchService){
+    function SearchController($log, SearchService, UserService){
         var vm = this;
+
+        vm.users = [];
+
+        UserService.get(function(data){
+            vm.users = data.elements;
+        }, function(errors){
+            $log.error(errors);
+        })
     }
 })();
